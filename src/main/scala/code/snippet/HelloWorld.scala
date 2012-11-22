600a61d70e5da45b15e95d61481c87c192141e99
Justin Heister <jheister@jheister-VPCCA1C5E.(none)>
2012-11-22 22:24:31 +0000

    Added lazyload page and error message to snippet

--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -4,6 +4,7 @@ package snippet
 import scala.xml.{NodeSeq, Text}
 import net.liftweb.util._
 import net.liftweb.common._
+import net.liftweb.http.S
 import java.util.Date
 import code.lib._
 import Helpers._
@@ -12,7 +13,10 @@ class HelloWorld {
   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
 
   // replace the contents of the element with id "time" with the date
-  def howdy = "#time *" #> date.map(_.toString)
+  def howdy = "#time *" #> {
+    S.error("This is an error message")
+    date.map(_.toString)
+  }
 
   /*
    lazy val date: Date = DependencyFactory.time.vend // create the date via factory
